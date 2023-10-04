b75ea7d7b210cf774bf060451acb505dc56017a8
Dell-User <Dell-User@DESKTOP-I2HH80B>
2023-10-04 11:02:48 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnArea = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        SuspendLayout()
+        ' 
+        ' btnArea
+        ' 
+        btnArea.Location = New Point(226, 87)
+        btnArea.Name = "btnArea"
+        btnArea.Size = New Size(112, 34)
+        btnArea.TabIndex = 0
+        btnArea.Text = " Answer"
+        btnArea.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(419, 40)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(419, 87)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(203, 42)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(135, 25)
+        Label1.TabIndex = 2
+        Label1.Text = "Enter the radius"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(btnArea)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnArea As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Reflection.Metadata
+
+Public Class Form1
+    Private Sub btnArea_Click(sender As Object, e As EventArgs) Handles btnArea.Click
+        Dim A As Double
+        Const Pi As Double = 22 / 7
+
+        Dim r As Double
+
+
+        r = Val(TextBox1.Text)
+
+        A = Pi * (r ^ 2)
+
+
+        TextBox2.Text = A
+    End Sub
+End Class
